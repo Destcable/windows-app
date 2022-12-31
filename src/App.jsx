2d9c18d3d08b34a6dd7c6dc8206525532cf33d66
Artem Pavlov <artem.pavlov@inthemelab.com>
2022-12-31 15:42:59 +0300

    [UPD] main auth

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
-import 
+import './app.css'
+import './pages/authorization/css/auth.css'
+import bg from './pages/authorization/img/bg.png';
+
 function App() {
+
   return (
-    <img src="logo.png"/>
+    <div className='auth-page' onselectstart="return false" onmousedown="return false">
+      <div className='main-page'>
+        <img src={bg} />
+        <div className='enter-template'/>
+      </div>
+    </div>
   );
 }
 
