0cbaff976371e292f51c61fc06c9c373703e664b
Artem Pavlov <artem.pavlov@inthemelab.com>
2022-12-31 18:07:10 +0300

    [UPD] start screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
 import './app.css'
 import './pages/authorization/css/auth.css'
-import bg from './pages/authorization/img/bg.png';
+import userIcon from './pages/user/icon-user/one.png';
 
 function App() {
 
   return (
-    <div className='auth-page' onselectstart="return false" onmousedown="return false">
+    <div className='auth-page'>
       <div className='main-page'>
-        <img src={bg} />
-        <div className='enter-template'/>
+        {/* <img src={bg} /> */}
+        <div className='enter-template'> 
+          <div className='template-block'>
+            <div className='left-template'>
+              <img src={userIcon} className='userIcon' />
+            </div>
+            <div className='right-template'>
+              <span className='userFieldAuth-title'>Artem Pavlov</span>
+              <p className='userFieldAuth-description'>user encrypted</p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
