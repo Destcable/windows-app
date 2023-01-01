0225aa3358e586973074ab924a3009ee8d947af2
Artem Pavlov <artem.pavlov@inthemelab.com>
2023-01-01 03:16:04 +0300

    [UPD] auth screen

--- src/pages/authorization/loginStart.jsx
+++ src/pages/authorization/loginStart.jsx
@@ -1,14 +1,18 @@
 import './css/authTwoStep.css';
-import userIconBig from '../user/icon-user/one_BIG.png'
 import { disableBodyScroll, enableBodyScroll } from 'body-scroll-lock';
+import userIconBig from '../user/icon-user/one_BIG.png';
+import arrowRightIcon from '../icons/arrow_right.svg';
 
 function LoginStartStep(elementScroll ) {
     disableBodyScroll(elementScroll)
     return( 
         <div className="authBlock">
             <img src={userIconBig} /><br />
-            <p>Artem Pavlov</p>
-            
+            <p className='userNameAuth'>Artem Pavlov</p>
+            <input className='inpPassword'></input>
+            <div className='arrowRightButton'>
+                <img src={arrowRightIcon} alt="arrowRightIcon" />
+            </div>
         </div>
     )
 }
